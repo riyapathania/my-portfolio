0e3c5d7c0fd40c35ba43dcbebc0dc6886deb2f3c
riyapathania <154575279+riyapathania@users.noreply.github.com>
2025-06-05 00:23:03 +0530

    Add QuantumBackground component

---
+++ components/QuantumBackground.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import StarfieldBackground from "./StarfieldBackground";
+import AtomSVG from "./AtomSVG";
+
+const QuantumBackground: React.FC = () => (
+  <>
+    <StarfieldBackground />
+    <AtomSVG />
+  </>
+);
+
+export default QuantumBackground;
